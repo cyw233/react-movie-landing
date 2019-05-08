a59b3870ba57cc2aa943acf712d616dbe470af37
Colin Wang <chenyangw233@gmail.com>
2019-05-08 20:43:52 +1000

    Make navbar collapse in smaller screens

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Navigation extends Component {
+  burgerToggle() {
+    let linksEl = document.querySelector('.narrowLinks');
+    if (linksEl.style.display === 'block') {
+      linksEl.style.display = 'none';
+    } else {
+      linksEl.style.display = 'block';
+    }
+  }
+
   render() {
     return (
       <div id="navigation" className="Navigation">
-        <ul>
-          <li>Community</li>
-          <li>Store</li>
-        </ul>
+        <nav>
+          <div className="navWide">
+            <div className="wideDiv">
+              <ul>
+                <li href="#">Trailers</li>
+                <li href="#">Photos</li>
+                <li href="#">Cast</li>
+              </ul>
+            </div>
+          </div>
+          <div className="navNarrow">
+            <i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
+            <div className="narrowLinks">
+              <a href="#" onClick={this.burgerToggle.bind(this)}>Trailers</a>
+              <a href="#" onClick={this.burgerToggle.bind(this)}>Photos</a>
+              <a href="#" onClick={this.burgerToggle.bind(this)}>Cast</a>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
